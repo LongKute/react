769f0ecc11202523b81d776d3513266a6073f4d3
longkute <luckim.long.5@gmail.com>
2024-11-30 14:44:56 +0700

    video 31
    Fragment, khai bao biến và sử dụng biến, import export file và sử dụng chúng

--- reactVibe/src/components/learn/MyComponent.jsx
+++ reactVibe/src/components/learn/MyComponent.jsx
@@ -1,6 +1,27 @@
+import "./style.css"
 const MyComponent = () => {
-    return (<div>Hello</div>);
-    
-  }
+    // const long = "longkute"
+    // const long = 25
+    // const long = true
+    // const long = undefined
+    // const long = null
+    // // array
+    const long = [1,2,3]
+    // const long = {
+    //     name: "longkute",
+    //     age: 24
+    // }
 
-  export default MyComponent
+  //Fragment
+  return (
+    <>
+      <div>Hello, {JSON.stringify(long)}</div> 
+      <div>{console.log("hello, Longkute")}</div>
+      <div className="child"
+      style={{backgroundColor:"Black" , borderRadius:"10px"}}
+      >Nice</div>
+    </>
+  );
+};
+
+export default MyComponent;
