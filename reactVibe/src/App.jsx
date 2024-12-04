85689242637b6ca0d3245c2cd30b1d2a24037dda
longkute <luckim.long.5@gmail.com>
2024-12-04 16:08:11 +0700

    video 47
    đã làm xong header và footer

--- reactVibe/src/App.jsx
+++ reactVibe/src/App.jsx
@@ -2,20 +2,21 @@ import { useState } from "react";
 import ToDoData from "./components/todo/toDoData.jsx";
 import ToDoNew from "./components/todo/toDoNew.jsx";
 import reactLogo from "./assets/react.svg";
-
+import Header from "./components/layout/header.jsx";
+import Footer from "./components/layout/footer.jsx";
 
 function App() {
+
+  const [toDoList, setToDoList] = useState([]);
   function GetRandomArbitrary(min, max) {
     return parseInt(Math.random() * (max - min + 1) + min);
   }
 
-  const [toDoList, setToDoList] = useState([]);
-  
+
   const deleteBtn = (id) => {
-    const updatedList = toDoList.filter((item) => item.id!== id);
+    const updatedList = toDoList.filter((item) => item.id !== id);
     setToDoList(updatedList);
-    
-  }
+  };
 
   const addNewToDo = (name) => {
     const newToDo = {
@@ -25,17 +26,21 @@ function App() {
     setToDoList([...toDoList, newToDo]);
   };
   return (
-    <div className="todo-container">
-      <div className="todo-title">Todo List</div>
-      <ToDoNew addNewToDo={addNewToDo} />
-      {toDoList.length > 0 ? (
-        <ToDoData toDoList={toDoList} deleteBtn={deleteBtn}/>
-      ) : (
-        <a className="logo" href="https://react.dev/" target="_blank">
-          <img src={reactLogo} className="logo_react" />
-        </a>
-      )}
-    </div>
+    <>
+      <Header />
+      <div className="todo-container">
+        <div className="todo-title">Todo List</div>
+        <ToDoNew addNewToDo={addNewToDo} />
+        {toDoList.length > 0 ? (
+          <ToDoData toDoList={toDoList} deleteBtn={deleteBtn} />
+        ) : (
+          <a className="logo" href="https://react.dev/" target="_blank">
+            <img src={reactLogo} className="logo_react" />
+          </a>
+        )}
+      </div>
+      <Footer/>
+    </>
   );
 }
 
