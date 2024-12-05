9ff7e8dfd4f50d5adab436385a124921188a1c08
longkute <luckim.long.5@gmail.com>
2024-12-05 15:18:16 +0700

    backup

--- reactVibe/server.js
+++ reactVibe/server.js
@@ -3,32 +3,50 @@ const cors = require("cors")
 const fs = require("fs")
 const path = require("path")
 const crypto = require("crypto")
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
 const app = express()
 app.use(express.json())
 app.use(cors())
-const port = 3007
-app.listen(port, () => {
-    console.log(` Server is running with port: ${port}`);
+const PORT = process.env.PORT || console.error("port error");
+
+app.listen(PORT, () => {
+    console.log(` Server is running with port: ${PORT}`);
     
 })
-function debug(message) {
-    return console.log(message);
-}
 
-function readWithPath(url){
-    const filePath = path.join(__dirname, url)
-    return JSON.parse(fs.readFileSync(filePath, "utf-8"))
-}
-function writeWithPath(url){
-    const filePath = path.join(__dirname, url)
-    return fs.writeFileSync(filePath, JSON.stringify(element, null, 2));
-}
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+   
+})
+.then(() => {
+    console.log("Connected to MongoDB")
+})
+.catch((error) => {console.error("Lỗi kết nối MongoDB:", error);
+})
+
+
+// function debug(message) {
+//     return console.log(message);
+// }
+
+// function readWithPath(url){
+//     const filePath = path.join(__dirname, url)
+//     return JSON.parse(fs.readFileSync(filePath, "utf-8"))
+// }
+// function writeWithPath(url){
+//     const filePath = path.join(__dirname, url)
+//     return fs.writeFileSync(filePath, JSON.stringify(element, null, 2));
+// }
 
 // 1.Xây dựng RESTful API với Node.js và Express:
 //    - Viết một server cơ bản với các endpoint (GET, POST, PUT, DELETE) để quản lý danh sách sản phẩm hoặc người dùng.
 //    - Tích hợp các route và middleware xử lý lỗi cơ bản.
-function auth() {
-    app.get("/user", () => {
+// function auth() {
+//     app.get("/user", () => {
 
-    })
-}
+//     })
+// }
